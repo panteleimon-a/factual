b02a13beddaf8a3955a9361c372d63a01bfd3952
Christos Mourelatos <mourcris@yahoo.gr>
2023-04-01 12:56:01 +0300

    Update request.py

--- bert/API/request.py
+++ bert/API/request.py
@@ -24,12 +24,13 @@ class fetch:
     def __init__(self, url):
         # for proxy in proxies:
           # try:
+            cookies = {'privacy-policy': '1,XXXXXXXXXXXXXXXXXXXXXX'}
             req = requests.get(
             url=url,
-            headers={"User-Agent": get_useragent()}
+            headers={"User-Agent": get_useragent()},
             # ,proxies={'http': proxy, 'https': proxy}
-            )
-            soup = BeautifulSoup(response,'html.parser',from_encoding=req)
+            cookies=cookies)
+#             soup = BeautifulSoup(response,'html.parser',from_encoding=req)
             # break
           # If the request failed, continue to the next proxy
           # except:
