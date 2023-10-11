da5942d7ba98e571276a7cc9775aa8833635e116
panteleimon-a <pantelisagg1@gmail.com>
2023-10-11 09:15:31 +0300

    Adding WalkPhase2

--- API/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from API.models import API
-
-# Register your models here.
-admin.site.register(API)
--- API/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from API.views import twitter_API
-
-urlpatterns = [
-    path('', twitter_API.as_view(), name="platform_API"),
-]
